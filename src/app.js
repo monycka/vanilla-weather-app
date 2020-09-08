23293f7774161aa228e670d09528e115e72a2ab2
monycka <monyckadc@gmail.com>
2020-09-08 16:52:27 -0700

    Added the icon description

--- src/app.js
+++ src/app.js
@@ -59,13 +59,27 @@ timeElement.innerHTML = time(currentTime);
 function currentWeather(response) {
   document.querySelector("#country").innerHTML = response.data.sys.country;
   document.querySelector("#current-city").innerHTML = response.data.name;
-  document.querySelector("#current-temperature").innerHTML = Math.round(response.data.main.temp);
-  document.querySelector("#daily-high").innerHTML = Math.round(response.data.main.temp_max);
-  document.querySelector("#daily-low").innerHTML = Math.round(response.data.main.temp_min);
-  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#daily-high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#daily-low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
-  document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#icon").setAttribute("src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 function searchingCity(event) {
@@ -77,7 +91,6 @@ function searchingCity(event) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
   let apiUrl = `${apiEndpoint}q=${cityName}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(currentWeather);
-  
 }
 
 let city = document.querySelector("#search-input");
@@ -85,17 +98,17 @@ city.addEventListener("submit", searchingCity);
 
 //Current Location
 function currentLocation(position) {
-navigator.geolocation.getCurrentPosition(currentLocation);
-let latitude = position.coords.latitude;
-let longitude = position.coords.longitude;
-let apiKey = "43d48c14e180f75f558e0def6bf829b0";
-let units = "imperial";
-let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  navigator.geolocation.getCurrentPosition(currentLocation);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "43d48c14e180f75f558e0def6bf829b0";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
-axios.get(apiUrl).then(currentWeather);
+  axios.get(apiUrl).then(currentWeather);
 }
- 
+
 let currentCity = document.querySelector("#current-location");
 currentCity.addEventListener("click", currentLocation);
 
