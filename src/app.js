1a648c878bf82e88d8683e6b7677c085e01f4801
monycka <monyckadc@gmail.com>
2020-09-07 18:39:12 -0700

    Added a country location

---
+++ src/app.js
@@ -0,0 +1,133 @@
+//Date
+function date(now) {
+  let year = now.getFullYear();
+  let date = now.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day}, ${month} ${date}, ${year}`;
+}
+
+let dateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+dateElement.innerHTML = date(currentDate);
+
+//Time
+function time(time) {
+  let hours = [
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+  ];
+  let hour = hours[time.getHours()];
+
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `Time: ${hour}:${minutes}`;
+}
+
+let timeElement = document.querySelector("#current-time");
+let currentTime = new Date();
+timeElement.innerHTML = time(currentTime);
+
+//Search City
+function currentWeather(response) {
+  document.querySelector("#country").innerHTML = response.data.sys.country;
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#daily-high").innerHTML = Math.round(response.data.main.temp_max);
+  document.querySelector("#daily-low").innerHTML = Math.round(response.data.main.temp_min);
+  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#weather-description").innerHTML = response.data.weather[0].description;
+  document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+}
+
+function searchingCity(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#searching");
+  let cityName = citySearch.value;
+  let apiKey = "43d48c14e180f75f558e0def6bf829b0";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${apiEndpoint}q=${cityName}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(currentWeather);
+}
+
+let city = document.querySelector("#search-input");
+city.addEventListener("submit", searchingCity);
+
+//Current Location
+function currentLocation(position) {
+navigator.geolocation.getCurrentPosition(currentLocation);
+let latitude = position.coords.latitude;
+let longitude = position.coords.longitude;
+let apiKey = "43d48c14e180f75f558e0def6bf829b0";
+let units = "imperial";
+let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(currentWeather);
+}
+ 
+let currentCity = document.querySelector("#user-location");
+currentCity.addEventListener("click", currentLocation);
+
+//City on Open
+function searchCity(city) {
+  let apiKey = "43d48c14e180f75f558e0def6bf829b0";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
+searchCity("San Francisco");
